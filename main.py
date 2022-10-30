30931c5901f4a3f499c9e5d9f5d80b03f242a488
Vladislav Gritsay <114249677+veles008@users.noreply.github.com>
2022-10-30 20:38:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = (int(input('Введите сумму: ')))
-new_list = []
-for value in per_cent.values():
-    deposit = int((money * value) / 100)
-    new_list.append(deposit)
-print(f'Список новых значений {new_list}')
-print(f'Максимальное значение {max(new_list)}')
+bileti = int(input('Введите число билетов которые хотите купить: '))
+spisok = []
+for i in range(1,bileti+1):
+    f = int(input(f'Введите возвраст {i} посетителя: '))
+    if f < 18:
+        spisok.append(0)
+    elif 18 <= f < 25:
+        spisok.append(990)
+    elif  f >= 25:
+        spisok.append(1390)
+if len(spisok)>3:
+    rasx = sum(spisok)- sum(spisok)/100*10
+    print(int(rasx), 'Сумма к оплате с учетом скидки 10%')
+else:
+    print(sum(spisok),' Ваша сумма к оплате')
